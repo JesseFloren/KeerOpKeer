2cf61d1008e37a5443d8a2f9245a4e82ebc4cb68
JesseFloren <jessefloren@gmail.com>
2025-01-19 14:14:32 +0100

    dev

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import {
-  useFonts,
-  OpenSans_300Light,
-  OpenSans_400Regular,
-  OpenSans_500Medium,
-  OpenSans_600SemiBold,
-  OpenSans_700Bold,
-  OpenSans_800ExtraBold,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold_Italic,
-  OpenSans_800ExtraBold_Italic,
-} from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
 import seedrandom from 'seedrandom';
 import Row from './components/Row';
